e3f2500e46baa25f7f8e15098133b5b4e3e86bb7
Felicia Hu <feliciahu1992_@hotmail.com>
2024-05-31 08:41:56 +0200

    chore: Update dependencies and remove unused code

---
+++ app/(auth)/_components/auth.provider.jsx
@@ -0,0 +1,111 @@
+'use client';
+
+import { auth } from '@/firebase.config';
+import {
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    updateProfile,
+} from 'firebase/auth';
+
+
+import { createContext, useContext, useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+
+export const AuthContext = createContext();
+
+const AuthContextProvider = ({ children }) => {
+
+  
+const [user, setUser] = useState(null)
+const [authLoaded, setAuthLoaded] = useState(false)
+
+
+useEffect(() => {
+  const unsub = onAuthStateChanged(auth, _user => {
+    setUser(_user)
+    setAuthLoaded(true)
+  })
+
+  return () => unsub()
+}, [])
+
+
+
+const register = async (values) => {
+  const toastId = toast.loading('Creating account...')
+
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, values.email, values.password)
+
+    if(!userCredential.user) {
+      throw new Error('Something went wrong!. Please try again.')
+    }
+    console.log(userCredential);
+
+    await updateProfile(userCredential.user, {
+      displayName: `${values.firstName} ${values.lastName}`
+    }) 
+
+    setUser(userCredential.user)
+
+    toast.success('Account created successfully',  { id: toastId })
+    
+    return userCredential.user.uid;
+
+  } catch (error) {
+    console.log(error.message);
+    console.log(error.code);
+    const message = error.code.split('/')[1].replace(/-/g, ' ')
+    toast.error(message || error.message, { id: toastId })
+  } 
+
+}
+
+
+const login = async (values) => {
+  const toastId = toast.loading('Signing in...')
+
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, values.email, values.password)
+
+    if(!userCredential.user) {
+      throw new Error('Something went wrong!. Please try again.')
+    }
+    console.log(userCredential);
+    const token = await userCredential.user.getIdToken()
+    console.log('token:', token)
+
+    toast.success('Logged in successfully',  { id: toastId })
+
+  } catch (error) {
+    console.log(error.message);
+    const message = error.code.split('/')[1].replace(/-/g, ' ')
+    toast.error(message || error.message, { id: toastId })
+  } 
+}
+
+
+const value = {
+  user,
+  setUser,
+  authLoaded,
+  register,
+  login
+}
+
+  return(
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthContextProvider
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context)
+        throw new Error('useAuth must be used within an AuthContextProvider');
+    return context;
+};
